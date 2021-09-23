5711fa71aa339338df3a4796edd44bfdb1bed225
Dorina Maraki <19mdori94@gmail.com>
2021-09-23 21:27:29 +0200

    feat(*): add fully responsive layout

--- main.js
+++ main.js
@@ -5,10 +5,8 @@ function showMenu(itemId, arrowId) {
   if ( itemDisplay === "none") {
     document.getElementById(itemId).style.display = "block";
     document.getElementById(arrowId).style.transform = "rotate(180deg)";
-    document.getElementById(arrowId).style.paddingRight = "9px";
   } else {
     document.getElementById(itemId).style.display = "none";
     document.getElementById(arrowId).style.transform = "rotate(0deg)";
-    document.getElementById(arrowId).style.paddingRight = "0px";
   }
 }
