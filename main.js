71ad97fcf386ab9e7025714be18ca6877244e6a2
marakid <19mdori94@gmail.com>
2021-09-23 14:33:57 +0200

    refactor(*): refactor menu popup

--- main.js
+++ main.js
@@ -1,43 +1,14 @@
 
-function showMenuProduct() {
-  let itemDisplay = document.getElementById("ddItem1").style.display;
+function showMenu(itemId, arrowId) {
+  let itemDisplay = document.getElementById(itemId).style.display;
   itemDisplay = itemDisplay || "none";
   if ( itemDisplay === "none") {
-    document.getElementById("ddItem1").style.display = "block";
-    document.getElementById("productArrow").style.transform = "rotate(180deg)";
-    document.getElementById("productArrow").style.paddingRight = "9px";
+    document.getElementById(itemId).style.display = "block";
+    document.getElementById(arrowId).style.transform = "rotate(180deg)";
+    document.getElementById(arrowId).style.paddingRight = "9px";
   } else {
-    document.getElementById("ddItem1").style.display = "none";
-    document.getElementById("productArrow").style.transform = "rotate(0deg)";
-    document.getElementById("productArrow").style.paddingRight = "0px";
-  }
-}
-
-function showMenuCompany() {
-  let itemDisplay = document.getElementById("ddItem2").style.display;
-  itemDisplay = itemDisplay || "none";
-  if ( itemDisplay === "none") {
-    document.getElementById("ddItem2").style.display = "block";
-    document.getElementById("companyArrow").style.transform = "rotate(180deg)";
-    document.getElementById("companyArrow").style.paddingRight = "9px";
-  } else {
-    document.getElementById("ddItem2").style.display = "none";
-    document.getElementById("companyArrow").style.transform = "rotate(0deg)";
-    document.getElementById("companyArrow").style.paddingRight = "0px";
-    
-  }
-}
-
-function showMenuConnect() {
-  let itemDisplay = document.getElementById("ddItem3").style.display;
-  itemDisplay = itemDisplay || "none";
-  if ( itemDisplay === "none") {
-    document.getElementById("ddItem3").style.display = "block";
-    document.getElementById("connectArrow").style.transform = "rotate(180deg)";
-    document.getElementById("connectArrow").style.paddingRight = "9px";
-  } else {
-    document.getElementById("ddItem3").style.display = "none";
-    document.getElementById("connectArrow").style.transform = "rotate(0deg)";
-    document.getElementById("connectArrow").style.paddingRight = "0px";
+    document.getElementById(itemId).style.display = "none";
+    document.getElementById(arrowId).style.transform = "rotate(0deg)";
+    document.getElementById(arrowId).style.paddingRight = "0px";
   }
 }
